cb0cb8d30867b70c3c2a2affd82c7d47fa914ba4
fakhriprayatna.putra <fakhriprayatna.putra@homecredit.co.id>
2023-09-15 11:09:54 +0700

    Update index.js

--- src/signaler/index.js
+++ src/signaler/index.js
@@ -6,8 +6,8 @@ const initializeSignaler = (io) => {
   const DEBUG = process.env.APP_STATE === DEV || PREPROD;
 
   // VARIABLES
-  let channels = {};
-  let rooms = {};
+  let stores = {};
+  let globalPeers = {};
 
   io.on("connection", async (socket) => {
     // INITIALIZE AFTER CONNECTION ESTABLISHED
@@ -16,13 +16,85 @@ const initializeSignaler = (io) => {
         console.log(
           `SOCKET ${socket.id} - has been connected`
         );
-      socket.emit("connection-success", (storeId) => {
-        // join the store id "socket room" to ease the signaling of the store scope
-        // this will mark the visitor count aswell
-        // join the chatsocket room as a "store" with store id
-        socket.join(storeId);
-      });
+      socket.emit(
+        "connection-success",
+        async ({ storeId, user }) => {
+          // join the store id "socket room" to ease the signaling of the store scope
+          // this will mark the visitor count aswell
+          // join the chatsocket room as a "store" with store id
+          const newUser = {
+            storeId: storeId,
+            socketId: socket.id,
+            userId: user ? user.userId : socket.id,
+            details: user,
+          };
+          const newPeer = {
+            user: newUser,
+            socket: socket,
+            socketId: socket.id,
+            storeId: storeId,
+          };
+
+          try {
+            // filter the stores object to get rid of double connection
+            const store = Object.entries(stores).some(
+              ([key, val]) => key === storeId
+            );
+
+            store &&
+              Object.entries(stores[storeId]).forEach(
+                ([key, val]) => {
+                  console.log(val);
+                  console.log(key);
+                  if (
+                    val.userId === newUser.userId &&
+                    val.socketId !== newUser.socketId
+                  )
+                    handleDisconnect(
+                      globalPeers[val.socketId]
+                    );
+                }
+              );
+
+            stores = {
+              ...stores,
+              [storeId]: {
+                ...stores[storeId],
+                [newUser.socketId]: newUser,
+              },
+            };
+            globalPeers = {
+              ...globalPeers,
+              [newPeer.socketId]: newPeer,
+            };
+
+            await socket.join(storeId);
+            socket.emit("new-connection-render", {
+              allJoinedUsers: stores[storeId],
+              user: newUser,
+            });
+            socket
+              .to(storeId)
+              .emit("new-connection-render", {
+                allJoinedUsers: stores[storeId],
+              });
+          } catch (e) {
+            DEBUG &&
+              console.log(
+                `SOCKET ${socket.id} - got error when joining the room: ${e}, disconnecting...`
+              );
+            await handleDisconnect(
+              globalPeers[newUser.socketId]
+            );
+          }
+        }
+      );
     })();
+
+    async function handleDisconnect(peer) {
+      if (!peer) return;
+      await peer.socket.disconnect();
+    }
 
     socket.on(
       "chat-send",
@@ -84,6 +156,24 @@ const initializeSignaler = (io) => {
         console.log(
           `SOCKET ${socket.id} - peer disconnected`
         );
+      try {
+        const removingPeer = globalPeers[socket.id];
+        delete stores[removingPeer.storeId][
+          removingPeer.socketId
+        ];
+        delete globalPeers[removingPeer.socketId];
+
+        socket
+          .to(removingPeer.storeId)
+          .emit("new-connection-render", {
+            allJoinedUsers: stores[removingPeer.storeId],
+          });
+      } catch (e) {
+        DEBUG &&
+          console.log(
+            `SOCKET ${socket.id} - error while cleaning the disconnected peer, error: ${e}`
+          );
+      }
     });
   });
 };
